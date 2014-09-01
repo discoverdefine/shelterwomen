7bec5d52593c2a94d6104f10aff6a0bfa8cb6cce
discoverdefine <christopher@discoverdefine.ca>
2014-09-01 17:24:28 -0400

    Resolutions

--- js/index.js
+++ js/index.js
@@ -90,7 +90,7 @@ function getListings_success(tx, results) {
     }
     if ($("#lst_directory_listview").length !== 0) {
         if (len === 0) {
-            $("#lst_directory_listview").append("<p><small>In order to run the application for the first time you will need to connect to the Internet (a WiFi connection is recommended to avoid additional data fees) and then SYNC with the online database.</small></p><a href=\"help.html\" data-ajax=\"false\" data-transition=\"flow\">See \"How do I get the latest data?\"</a>");
+            $("#lst_directory_listview").append("<p><small>In order to run the application for the first time you will need to connect to the Internet (a WiFi connection is recommended to avoid additional data fees) and then SYNC with the online database.</small></p><a href=\"help.html\" data-ajax=\"false\" data-transition=\"flow\">See \"How do I get started?\"</a>");
         }
         $("#lst_directory_listview").listview('refresh');
     }
