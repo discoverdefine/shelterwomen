83fc9501948da84b30ac9e9d0153065ed1e4fe95
discoverdefine <christopher@discoverdefine.ca>
2014-09-14 23:30:08 -0400

    Ajax

--- js/authorize.js
+++ js/authorize.js
@@ -8,17 +8,17 @@ function onDeviceReady() {
     func_alert_the_guard();
 }
 
-function func_alert_the_guard(activation_key) {
+function func_alert_the_guard(act_key) {
 	tmp_url = "https://shelterforwomen.ca/admin/api.php";
 	//tmp_data = "action=validate&key=" + api_key + "&activation_key=" + activation_key;
-	tmp_data = { action:"validate", key: api_key, activation_key: activation_key };
+	//tmp_data = { action:"validate", key: api_key, activation_key: activation_key };
 	alert("Woke the guard...");
     $.post(
         tmp_url,
 		{
-			action:"validate",
+			action: "validate",
 			key: api_key,
-			activation_key: activation_key
+			activation_key: act_key
 		},
 		function(response) {
 			alert("Response: " + response);
