8317676c699bfc125519f97669adcd8f446b1312
discoverdefine <christopher@discoverdefine.ca>
2014-09-14 22:14:18 -0400

    Ajax trial

--- js/authorize.js
+++ js/authorize.js
@@ -8,15 +8,22 @@ function onDeviceReady() {
 }
 
 function func_alert_the_guard(activation_key) {
+	tmp_url = "https://shelterforwomen.ca/admin/api.php";
+	tmp_data = "action=validate&key=" + api_key + "&activation_key=" + $activation_key;
+	alert("Woke the guard...");
     $.ajax({
-        url: 'https://shelterforwomen.ca/admin/api.php?action=validate&key=' + api_key + '&activation_key=' + $activation_key,
-		async: true,
-        dataType: "text"
-    }).done(function(response) {
-		
-		alert(response);
+        url: tmp_url,
+		data: tmp_data,
+		type: "post",
+		success: function(response, textStatus, jqXHR) {
+			alert("Response: " + response);
+		},
+		error: function(jqXHR, textStatus, errorThrown) {
+			alert("Error - textStatus: " + textStatus);
+			alert("Error - errorThrown: " + errorThrown);
+		}
+    })
 
-    });
 }
 
 function transactionError() {
