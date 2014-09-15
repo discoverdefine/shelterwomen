ed563f0d470c5f76b7ad6441cf784f582bba518b
discoverdefine <christopher@discoverdefine.ca>
2014-09-15 01:22:27 -0400

    Activate redirect

--- js/authorize.js
+++ js/authorize.js
@@ -8,10 +8,35 @@ function onDeviceReady() {
     func_alert_the_guard();
 }
 
+//Checks to see if activation key has been saved locally and if so, retrieves it, otherwise redirects to activation screen
+function func_too_legit() {
+	//Get the root for the local server
+    nativeLocalRootPath = fileSystem.root.toURL();
+    //Build full path to activation key file from the root down
+    var local_file_full_path = nativeLocalRootPath + "casvaw/activation.key";
+    //Check for existence of activation key file on local server
+    fileSystem.root.getFile(local_file_full_path, {create: false, exclusive: false}, function() {
+		//If file exists then grab the activation key and compare it to the one on the server
+		local_file_full_path.file(function(file) {
+			var reader = new FileReader();
+			reader.onloadend = function(e) {
+				alert(this.result);
+				//console.log("Text is: "+this.result);
+				//document.querySelector("#textArea").innerHTML = this.result;
+			}
+			reader.readAsText(file);
+		}, function() {
+		//If file doesn't exist, the user has not activated their app so redirect them to the activation page
+			window.location.href="activate.html";
+			return; //Added just to be safe
+		});
+	});
+};
+
 function func_alert_the_guard(act_key) {
 	alert("Woke the guard...");
 	
-	act_key = "f84868ae448b0e0b558e4c46a195a76b";
+	//act_key = "f84868ae448b0e0b558e4c46a195a76b";
 	
 	var request = $.ajax({
 		url: "https://shelterforwomen.ca/admin/api.php",
