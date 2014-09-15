b66d260263d75d8628259d1c168baf944120f6fe
discoverdefine <christopher@discoverdefine.ca>
2014-09-15 02:07:08 -0400

    Redirect

--- js/authorize.js
+++ js/authorize.js
@@ -25,26 +25,26 @@ function set_root_directory() {
 //Checks to see if activation key has been saved locally and if so, retrieves it, otherwise redirects to activation screen
 function func_too_legit() {
 alert("Too legit...");
-	//Get the root for the local server
-    nativeLocalRootPath = fileSystem.root.toURL();
-    //Build full path to activation key file from the root down
-    var local_file_full_path = nativeLocalRootPath + "casvaw/activation.key";	
-alert("Path: " + local_file_full_path);
-	//Check for existence of activation key file on local server
-    fileSystem.root.getFile(local_file_full_path, {create: false, exclusive: false}, function() {
-		//If file exists then grab the activation key and compare it to the one on the server
-		local_file_full_path.file(function(file) {
-			var reader = new FileReader();
-			reader.onloadend = function(e) {
-				alert(this.result);
-				//console.log("Text is: "+this.result);
-				//document.querySelector("#textArea").innerHTML = this.result;
-			}
+	
+	window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, function(fileSystem) {
+alert("Path: " + fileSystem.root + "casvaw/activation.key");
+		fileSystem.root.getFile("casvaw/activation.key", {create: false, exclusive: false}, function() {
+alert("Found the file?");
+			//If file exists then grab the activation key and compare it to the one on the server
+			fileSystem.root + "casvaw/activation.key".file(function(file) {
+				var reader = new FileReader();
+				reader.onloadend = function(e) {
+					alert(this.result);
+					//console.log("Text is: "+this.result);
+					//document.querySelector("#textArea").innerHTML = this.result;
+				}
 			reader.readAsText(file);
-		}, function() {
-		//If file doesn't exist, the user has not activated their app so redirect them to the activation page
-			window.location.href="activate.html";
-			return; //Added just to be safe
+			}, function() {
+alert("No file");
+				//If file doesn't exist, the user has not activated their app so redirect them to the activation page
+				window.location.href="activate.html";
+				return; //Added just to be safe
+			});
 		});
 	});
 };
