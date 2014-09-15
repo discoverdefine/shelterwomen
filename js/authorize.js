fc915860c6ab12b0d02a8d0351bb53c5f17e6804
discoverdefine <christopher@discoverdefine.ca>
2014-09-14 22:20:07 -0400

    Alert

--- js/authorize.js
+++ js/authorize.js
@@ -4,14 +4,16 @@ var api_key = 'ad37743918e82143d5c18f072e932d4f';
 document.addEventListener("deviceready", onDeviceReady, false);
 
 function onDeviceReady() {
+	alert("Alert the guard!");
     func_alert_the_guard();
 }
 
 function func_alert_the_guard(activation_key) {
-	tmp_url = "https://shelterforwomen.ca/admin/api.php";
-	tmp_data = "action=validate&key=" + api_key + "&activation_key=" + $activation_key;
+	//tmp_url = "https://shelterforwomen.ca/admin/api.php";
+	//tmp_data = "action=validate&key=" + api_key + "&activation_key=" + $activation_key;
 	alert("Woke the guard...");
-    $.ajax({
+    /*
+	$.ajax({
         url: tmp_url,
 		data: tmp_data,
 		type: "post",
@@ -23,7 +25,7 @@ function func_alert_the_guard(activation_key) {
 			alert("Error - errorThrown: " + errorThrown);
 		}
     })
-
+	*/
 }
 
 function transactionError() {
