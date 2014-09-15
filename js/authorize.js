5ffb6892b3ccbae496caeb76c0f82bb41be51153
discoverdefine <christopher@discoverdefine.ca>
2014-09-14 20:31:25 -0400

    Added authorize function

--- js/authorize.js
+++ js/authorize.js
@@ -4,7 +4,7 @@ var api_key = 'ad37743918e82143d5c18f072e932d4f';
 document.addEventListener("deviceready", onDeviceReady, false);
 
 function onDeviceReady() {
-    
+    func_alert_the_guard();
 }
 
 function func_alert_the_guard(key_activation) {
