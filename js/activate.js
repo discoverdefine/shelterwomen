fd04c2b915402296355ad488e86e4f6ca93ae724
discoverdefine <christopher@discoverdefine.ca>
2014-09-16 12:17:55 -0400

    Count access

--- js/activate.js
+++ js/activate.js
@@ -12,20 +12,32 @@ function onDeviceReady() {
 function getActive() {
 	alert('Submitting form...');
 	
-	if ( $('#email').val().length > 0 && $('#sync_key').val().length > 0 ) {
+	$email = $('#email').val();
+	$sync_key = $('#sync_key').val();
+	
+	//if ( $('#email').val().length > 0 && $('#sync_key').val().length > 0 ) {
+	if ( $email.length > 0 && $sync_key.length > 0 ) {
 		
-	alert('Email: ' + $('#email').val());
-	alert('Sync Key: ' + $('#sync_key').val());
+	alert('Email: ' + $email);
+	alert('Sync Key: ' + $sync_key);
 		
 		// Send data to server through the ajax call
 		// action is functionality we want to call and outputJSON is our data
 		$.ajax({
-			url: 'https://shelterforwomen.ca/admin/api.php?action=activate&key=' + api_key + '&sync_key=' + $('#sync_key').val() + '&email=' + $('#email').val(),
+			url: 'https://shelterforwomen.ca/admin/api.php?action=activate&key=' + api_key + '&sync_key=' + $sync_key + '&email=' + $email,
 			//data: {action : 'activate', formData : $('#activate-user').serialize()},
 			//data: {action : 'activate', formData : $('#activate-user').serialize()},
 			//type: 'post',                  
 			async: 'true',
-			dataType: 'json'
+			dataType: 'json',
+			beforeSend: function() {
+				// This callback function will trigger before data is sent
+				$.mobile.loading("show"); // This will show ajax spinner
+			},
+			complete: function() {
+				// This callback function will trigger on data sent/received complete
+				$.mobile.loading("hide"); // This will hide ajax spinner
+			}
 		}).done( function(data) {
 				
 			if ( data.status ) {
