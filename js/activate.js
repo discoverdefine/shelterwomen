0da827173840ae8c06ae9173d58beb67f3547fe0
discoverdefine <christopher@discoverdefine.ca>
2014-09-16 13:37:29 -0400

    activate clean up

--- js/activate.js
+++ js/activate.js
@@ -12,24 +12,20 @@ function onDeviceReady() {
 function getActive() {
 	alert('Submitting form...');
 	
-	$email = $('#email').val();
-	$sync_key = $('#sync_key').val();
+	v_email = $('#email').val();
+	v_sync_key = $('#sync_key').val();
 	
 	//if ( $('#email').val().length > 0 && $('#sync_key').val().length > 0 ) {
-	if ( $email.length > 0 && $sync_key.length > 0 ) {
+	if ( v_email.length > 0 && v_sync_key.length > 0 ) {
 		
-	alert('Email: ' + $email);
-	alert('Sync Key: ' + $sync_key);
+	alert('Email: ' + v_email);
+	alert('Sync Key: ' + v_sync_key);
 		
 		// Send data to server through the ajax call
 		// action is functionality we want to call and outputJSON is our data
 		$.ajax({
-			url: 'https://shelterforwomen.ca/admin/api.php?action=activate&key=' + api_key + '&sync_key=' + $sync_key + '&email=' + $email,
-			//data: {action : 'activate', formData : $('#activate-user').serialize()},
-			//data: {action : 'activate', formData : $('#activate-user').serialize()},
-			//type: 'post',                  
+			url: 'https://shelterforwomen.ca/admin/api.php?action=activate&key=' + api_key + '&sync_key=' + v_sync_key + '&email=' + v_email,
 			async: 'true',
-			//dataType: 'json',
 			beforeSend: function() {
 				// This callback function will trigger before data is sent
 				$.mobile.loading("show"); // This will show ajax spinner
@@ -38,11 +34,21 @@ function getActive() {
 				// This callback function will trigger on data sent/received complete
 				$.mobile.loading("hide"); // This will hide ajax spinner
 			}
-		}).done( function(data) {
-				
-			if ( data.status ) {
-				alert(data.sync_key);
-				alert(data.activation_key);
+		}).done( function(activate_response) {
+			
+			if ( !empty(activate_response) ) {
+			
+			alert('Got a response');
+			
+				a_activate_response = json_decode(activate_response, true); // decode
+			
+			b_is_array.isArray() = a_activate_response;
+			
+			alert("Is array: " + b_is_array);
+			
+				//if ( data.status ) {
+					//alert(data.sync_key);
+					//alert(data.activation_key);
 			}
 			else {
 				alert("Activation failed");
